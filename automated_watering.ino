849e052f9042fb64df7c153308dbb89fcca17234
Cedric <alecucedric@gmail.com>
2024-10-15 15:03:02 +0300

    watering sensor works and displays on LCD data

--- automated_watering.ino
+++ automated_watering.ino
@@ -1,28 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal_I2C.h>
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-
-int moistureSensorPin = 7;  // Pin where D0 is connected
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+int moistureSensorPin = 0;  // Pin where D0 is connected
 int moistureState = 0;
 
 void setup() {
+  // put your setup code here, to run once:
+  //LCD
+  lcd.init();         // initialize the lcd
+  lcd.backlight();    // Turn on the LCD screen backlight
+  //moisture sensor
   pinMode(moistureSensorPin, INPUT);  // Set the D0 pin as input
   Serial.begin(9600);  // Start the serial communication for monitoring
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   moistureState = digitalRead(moistureSensorPin);  // Read the digital output from D0
-  
+  lcd.setCursor(0, 0);
+
   if (moistureState == HIGH) {
-    Serial.println("Soil is dry");
+    lcd.print("Soil is dry");
   } else {
-    Serial.println("Soil is wet");
+    lcd.print("Soil is wet");
   }
   
   delay(1000);  // Wait for a second before reading again
